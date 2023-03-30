a9657931b372ba5c6f13df3198bee29cffc7b2b0
yehvaed <jaroslaw.walach@gmail.com>
2023-03-30 21:47:15 +0200

    first commit

---
+++ .zshrc
@@ -0,0 +1,42 @@
+.init() {
+	has 'fzf' source 'Aloxaf/fzf-tab' wait'idle'
+	has 'fzf' source 'joshskidmore/zsh-fzf-history-search' wait'idle'
+
+	has 'fasd' source '@omzp/fasd' wait'idle'
+	has 'exa' source 'MohamedElashri/exa-zsh' wait'idle'
+
+	has 'terraform' source "@omzp/terraform" wait'idle'
+	has 'aws' source "@omzp/aws" wait'idle'
+	has 'ansible' source "@omzp/ansible" wait'idle'
+	has 'nomad' source "@omzp/nomad" wait'idle'
+	has 'nomad' source "@omzp/docker" wait'idle'
+
+	has 'npm' source '@omzp/npm' wait'idle'
+	has 'yarn' source '@omzp/yarn' wait'idle'
+
+	has 'git' source "@omzp/git" wait'idle'
+	has 'gh' source "@omzp/gh" wait'idle'
+
+	source 'hlissner/zsh-autopair' wait'idle'
+	source 'z-shell/F-Sy-H' wait'idle'
+	source 'olets/zsh-abbr' wait'idle'
+	source 'MichaelAquilina/zsh-you-should-use' wait'idle'
+	source 'zpm-zsh/undollar' wait'idle'
+	source '@omzp/fancy-ctrl-z' wait'idle'
+
+	source "@omzl/history"
+	source 'ahmubashshir/zinsults'
+	source "romkatv/powerlevel10k" pick"powerlevel10k.zsh-theme"
+}
+
+() {
+	# ==> @omzl/history
+	export HISTSIZE=5000000
+	export SAVEHIST=1000000
+	export HISTFILE=${HOME}/.zhistory
+
+	# ==> MichaelAquilina/zsh-you-should-use
+	export YSU_MESSAGE_POSITION="after"
+}
+
+source init.zsh
---
+++ init.zsh
@@ -0,0 +1,53 @@
+#!/usr/bin/env zsh
+
+# https://wiki.zshell.dev/community/zsh_plugin_standard#zero-handling
+0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+typeset -gA ZUTILS
+: ${ZUTILS[HOME_DIR]:=${0:h}}
+: ${ZUTILS[PLUGINS_DIR]:=${0:h}/plugins}
+: ${ZSH_CACHE_DIR:=${HOME}/.cache}
+
+path+=( "/usr/local/bin" )
+
+local autoload_dirs=( ${0:h}/core/**/*(/) ${0:h}/syntax )
+fpath+=( ${(@)autoload_dirs} "${HOME}/.cache/completions" )
+
+for dir in ${(@)autoload_dirs}; do
+	autoload -Uz ${dir}/*(.ND:t) > /dev/null
+done
+
+local -a pmspec=(
+	# "f" # supports the functions/ subdirectory,
+	"i" # the zsh_loaded_plugins activity indicator,
+	"s" # the PMSPEC global parameter itself (i.e.: should be always present).
+)
+export PMSPEC="${(j::)pmspec}"
+unset pmspec
+
+autoload colors && colors
+autoload compinit && compinit -u
+
+functions[.zpack_bootstrap]=$(
+  echo ${functions[.init]} \
+	| sed 's/source/@source/g' \
+	| sed 's/has/@has/g'
+)
+
+.init() {
+	source "romkatv/zsh-defer"
+}
+
+functions[.zpack_bootstrap_base]=$(
+  echo ${functions[.init]} \
+	| sed 's/source/@source/g' \
+	| sed 's/has/@has/g'
+)
+
+.zpack_bootstrap_base
+.zpack_bootstrap
+
+[[ -n "${ZU_EVENT_NEW_PLUGINS}" ]] && echo "\nYour shell is up to date 🎉" && unset ZU_EVENT_NEW_PLUGINS
+
+autoload compinit && compinit -u
